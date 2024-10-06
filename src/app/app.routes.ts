d716f1fbc24bf825831c2a177fb436d699362d50
Silas Owu <owusilasyp00@gmail.com>
2024-10-06 13:21:18 -0700

    🚚 Chore: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginComponent } from './views/auth/login/login.component';
 
 export const routes: Routes = [
@@ -12,7 +13,21 @@ export const routes: Routes = [
     path: 'auth',
     component: AppComponent,
     children: [
-      { path: 'login', component: LoginComponent },
+      {
+        path: 'login',
+        component: LoginComponent,
+        children:[
+          {
+            path: '',
+            component: LoginFormComponent
+          },
+          {
+            path: 'forgot-password',
+            loadComponent: () => import('./components/forgot-password/forgot-password.component')
+             .then(m => m.ForgotPasswordComponent)
+          }
+        ]
+      },
       {
         path: 'register',
         loadComponent:()=> import('./views/auth/register/register.component')
---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,93 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { DynamicInputFieldComponent } from '../../shared/dynamic-input-field/dynamic-input-field.component';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { ToastComponent } from '../../shared/toast/toast.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [
+    MatDividerModule,
+    DynamicInputFieldComponent,
+    ReactiveFormsModule,
+    CommonModule,
+    ToastComponent,
+    ProgressSpinnerModule,
+    LoaderComponent,
+    RouterModule
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class LoginFormComponent {
+  loginForm!: FormGroup;
+  showToast = false;
+  toastMessage = '';
+  isError = false;
+  isLoading = false;
+
+  loginInputConfig = [
+    {
+      name: 'email',
+      label: 'Email Address',
+      type: 'email',
+      placeholder: 'Enter address here',
+      validators: [Validators.required, Validators.email]
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      type: 'password',
+      placeholder: '******',
+      validators: [Validators.required, Validators.minLength(8)]
+    },
+  ];
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({});
+    this.loginInputConfig.forEach(config => {
+      this.loginForm.addControl(config.name, this.fb.control('', config.validators));
+    });
+
+
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Login details:', JSON.stringify(this.loginForm.value));
+      this.isLoading = true;
+      this.cdr.detectChanges();
+      setTimeout(() => {
+        this.showToast = true;
+        this.isError = false;
+        this.toastMessage = 'Login successful!';
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      }, 2000);
+    } else {
+      this.loginForm.markAllAsTouched();
+      this.toastMessage = 'Invalid input(s), please all required fields';
+      this.isError = true;
+      this.showToast = true;
+    }
+  }
+
+  onErrorMessageChange(message: string) {
+    if (message) {
+      this.showToast = true;
+      this.isError = true;
+      this.toastMessage = message;
+    } else {
+      this.showToast = false;
+    }
+  }
+}
